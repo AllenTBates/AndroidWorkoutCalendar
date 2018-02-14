913b568017bab38ad16b9de45ab55bab3d53e383
AllenTBates <gamecockazul@gmail.com>
2018-02-13 23:03:35 -0500

    Initialized Calendar View object in Month Activity

--- app/src/main/java/com/example/allen/workoutcalendar/MonthActivity.java
+++ app/src/main/java/com/example/allen/workoutcalendar/MonthActivity.java
@@ -2,12 +2,17 @@ package com.example.allen.workoutcalendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 public class MonthActivity extends AppCompatActivity {
+
+    private CalendarView cvMonth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_month);
+
+        cvMonth = findViewById(R.id.cv_month_cal);
     }
 }
